4b99b727d4987a3b000b00f0ec57a48914be744c
billie <billie.rolih@gmail.com>
2016-09-02 22:36:41 -0700

    first lazy commit ,ex 14

---
+++ python-learn/14_ListRemoveDup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/Python
+
+#billiescodes 2016
+#Write a program (function!) that takes a list and returns a new list that contains 
+# all the elements of the first list minus all the duplicates.
+
+def main():
+
+
+
+#End of file stuff
+if __name__=='__main__':
+	main()
