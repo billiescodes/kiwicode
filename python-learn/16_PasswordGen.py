469e2581ee38f535ca5760868b42823a55fb6cab
billie <billie.rolih@gmail.com>
2016-09-05 13:11:08 -0700

    ex.16 first commit

---
+++ python-learn/16_PasswordGen.py
@@ -0,0 +1,17 @@
+#!/usr/bin/Python
+
+#billiescodes 2016
+#Write a password generator in Python
+# step 1: build a 4-char password gen of numbers
+# step 2: add letters and characters 
+# step 4: ask user how long you want the password gen to be
+
+
+def main():
+
+	print "\n\t\t\t ~ Password Generator ~"
+
+
+
+if __name__=='__main__':
+	main()	
